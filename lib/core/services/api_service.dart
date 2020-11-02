29e263bef359a764ae6278496ff5fc91d171a9e6
Giles Correia Morton <gilescmsolutions@gmail.com>
2020-11-02 00:02:24 +0000

    Missed from earlier commits

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:pingna/core/models/product/product.dart';
+import 'package:pingna/core/models/product/product_type.dart';
 
 import 'package:pingna/core/models/user.dart';
 import 'package:pingna/core/models/shop/shop.dart';
@@ -10,6 +12,13 @@ import 'package:pingna/core/models/shop/shop_label.dart';
 class PingnaApi {
   Future<User> login(String email, String password) {
     return Future.value(User(1, email: email));
+  }
+
+  Future<List<ShopType>> shopTypes() async {
+    final List shopTypesList = jsonDecode(await getJson('shop_types'));
+    return shopTypesList.map<ShopType>((item) {
+      return ShopType.fromMap(item);
+    }).toList();
   }
 
   Future<List<Shop>> shops() async {
@@ -22,10 +31,23 @@ class PingnaApi {
     return labelList.map<ShopLabel>((item) => ShopLabel.fromMap(item)).toList();
   }
 
-  Future<List<ShopType>> shopTypes() async {
-    final List shopTypesList = jsonDecode(await getJson('shop_types'));
-    return shopTypesList.map<ShopType>((item) {
-      return ShopType.fromMap(item);
+  Future<List<ProductType>> productTypes(int shopId) async {
+    final List productTypesList = jsonDecode(await getJson('product_types'));
+    final allProductTypes = productTypesList.map<ProductType>((item) {
+      return ProductType.fromMap(item);
+    });
+
+    return allProductTypes.where((item) => item.shopId == shopId).toList();
+  }
+
+  Future<List<Product>> products(int productTypeId) async {
+    final List productsList = jsonDecode(await getJson('products'));
+    final allProducts = productsList.map<Product>((item) {
+      return Product.fromMap(item);
+    });
+
+    return allProducts.where((item) {
+      return item.productTypeId == productTypeId;
     }).toList();
   }
 
