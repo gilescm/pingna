0419a1824db2872cbf5e971ea6e8c65955991ed8
Giles Correia Morton <gilescmsolutions@gmail.com>
2020-11-03 21:33:07 +0000

    Make size aware widget less expensive
    RenderProxyBox is called on every build of the child and its descendents, which makes for a more expensive widget. We don't need the size past this point so I've updated the widget to use a simpler method of size calculation.

--- lib/resources/widgets/utilities/measured_size_widget.dart
+++ lib/resources/widgets/utilities/measured_size_widget.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 
 typedef void OnWidgetSizeChange(Size size);
 
-class MeasureSizeRenderObject extends RenderProxyBox {
-  Size oldSize;
-  final OnWidgetSizeChange onChange;
-
-  MeasureSizeRenderObject(this.onChange);
-
-  @override
-  void performLayout() {
-    super.performLayout();
-
-    Size newSize = child.size;
-    if (oldSize == newSize) return;
-
-    oldSize = newSize;
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      onChange(newSize);
-    });
-  }
-}
-
-class MeasureSize extends SingleChildRenderObjectWidget {
+class MeasureSize extends StatefulWidget {
+  final Widget child;
   final OnWidgetSizeChange onChange;
 
   const MeasureSize({
     Key key,
     @required this.onChange,
-    @required Widget child,
-  }) : super(key: key, child: child);
+    @required this.child,
+  }) : super(key: key);
 
   @override
-  RenderObject createRenderObject(BuildContext context) {
-    return MeasureSizeRenderObject(onChange);
+  _MeasureSizeState createState() => _MeasureSizeState();
+}
+
+
+class _MeasureSizeState extends State<MeasureSize> {
+  @override
+  Widget build(BuildContext context) {
+    SchedulerBinding.instance.addPostFrameCallback(postFrameCallback);
+    return Container(
+      key: widgetKey,
+      child: widget.child,
+    );
+  }
+
+  var widgetKey = GlobalKey();
+  var oldSize;
+
+  void postFrameCallback(_) {
+    var context = widgetKey.currentContext;
+    if (context == null) return;
+
+    var newSize = context.size;
+    if (oldSize == newSize) return;
+
+    oldSize = newSize;
+    widget.onChange(newSize);
   }
 }
